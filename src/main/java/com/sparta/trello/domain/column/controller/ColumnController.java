29e8930da583ad6d02b78cbc84672120e3b90c2c
DAchoMI <dachomi97@gmail.com>
2024-03-19 10:24:28 +0900

    :sparkles: 컬럼 이름 수정 API 구현

--- src/main/java/com/sparta/trello/domain/column/controller/ColumnController.java
+++ src/main/java/com/sparta/trello/domain/column/controller/ColumnController.java
@@ -1,12 +1,14 @@
 package com.sparta.trello.domain.column.controller;
 
-import com.sparta.trello.domain.column.dto.ColumnCreateRequest;
+import com.sparta.trello.domain.column.dto.CreateColumnRequest;
+import com.sparta.trello.domain.column.dto.ModifyColumnNameRequest;
 import com.sparta.trello.domain.column.service.ColumnService;
 import com.sparta.trello.global.dto.CommonResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,11 +26,24 @@ public class ColumnController {
     @PostMapping("/boards/{boardId}/columns")
     public ResponseEntity<CommonResponse<Void>> createColumn(
         @PathVariable Long boardId,
-        @RequestBody ColumnCreateRequest request
+        @RequestBody CreateColumnRequest request
     ) {
         columnService.createColumn(boardId, request);
 
         return ResponseEntity.status(HttpStatus.CREATED.value()).body(
+            CommonResponse.<Void>builder()
+                .httpCode(HttpStatus.CREATED.value()).build());
+    }
+
+    @PatchMapping("/boards/{boardId}/columns/{columnId}")
+    public ResponseEntity<CommonResponse<Void>> modifyColumnName(
+        @PathVariable Long boardId,
+        @PathVariable Long columnId,
+        @RequestBody ModifyColumnNameRequest request
+    ) {
+        columnService.modifyColumnName(columnId, request);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT.value()).body(
             CommonResponse.<Void>builder()
                 .httpCode(HttpStatus.CREATED.value()).build());
     }

3bdd130f752006fc0186ae8add2a5f96648f2c95
DAchoMI <dachomi97@gmail.com>
2024-03-19 10:24:28 +0900

    :sparkles: 컬럼 이름 수정 API 구현

--- src/main/java/com/sparta/trello/domain/column/service/ColumnService.java
+++ src/main/java/com/sparta/trello/domain/column/service/ColumnService.java
@@ -2,13 +2,15 @@ package com.sparta.trello.domain.column.service;
 
 import com.sparta.trello.domain.board.entity.Board;
 import com.sparta.trello.domain.board.repository.BoardRepository;
-import com.sparta.trello.domain.column.dto.ColumnCreateRequest;
+import com.sparta.trello.domain.column.dto.CreateColumnRequest;
+import com.sparta.trello.domain.column.dto.ModifyColumnNameRequest;
 import com.sparta.trello.domain.column.entity.Columns;
 import com.sparta.trello.domain.column.repository.ColumnRepository;
 import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -18,15 +20,29 @@ public class ColumnService {
     private final ColumnRepository columnRepository;
     private final BoardRepository boardRepository;
 
-    public void createColumn(Long boardId, ColumnCreateRequest request) {
-        Board board = validateBoard(boardId);
+    @Transactional
+    public void createColumn(Long boardId, CreateColumnRequest request) {
+        Board board = validateExistBoard(boardId);
 
         columnRepository.save(
             Columns.builder().columnName(request.getColumnName()).sequence(
                 request.getSequence()).board(board).build());
     }
 
-    private Board validateBoard(Long boardId) {
+    @Transactional
+    public void modifyColumnName(Long columnId, ModifyColumnNameRequest request) {
+        Columns columns = validateExistColumn(columnId);
+
+        columns.setColumnName(request.getColumnName());
+    }
+
+    private Columns validateExistColumn(Long columnId) {
+        return columnRepository.findById(columnId).orElseThrow(
+            () -> new NoSuchElementException("해당 컬럼을 찾을 수 없습니다.")
+        );
+    }
+
+    private Board validateExistBoard(Long boardId) {
         return boardRepository.findById(boardId).orElseThrow(
             () -> new NoSuchElementException("해당 보드를 찾을 수 없습니다.")
         );

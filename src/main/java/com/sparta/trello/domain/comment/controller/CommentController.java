fedae001effdca9bd1e480087ea8f7daadd55a70
noeyodeel <sko8044@gmail.com>
2024-03-21 14:26:03 +0900

    :recycle: ResponseEntity 구조 통일시킴

--- src/main/java/com/sparta/trello/domain/comment/controller/CommentController.java
+++ src/main/java/com/sparta/trello/domain/comment/controller/CommentController.java
@@ -6,11 +6,10 @@ import com.sparta.trello.global.dto.CommonResponse;
 import com.sparta.trello.global.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,37 +19,50 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/cards/{cardId}/comments")
+@RequestMapping("/api/v1/boards/{boardId}/cards/{cardId}/comments")
 @RestController
 public class CommentController {
 
     private final CommentService commentService;
 
     @PostMapping()
-    public CommonResponse createComment(@PathVariable Long cardId,@RequestBody CommentRequest request,
+    public ResponseEntity<CommonResponse> createComment(@PathVariable Long cardId,
+        @PathVariable Long boardId,
+        @RequestBody CommentRequest request,
         @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        commentService.createComment(cardId,request,userDetails.getUser());
+        commentService.createComment(cardId, boardId, request, userDetails.getUser());
 
-        return CommonResponse.builder()
-            .httpCode(201)
-            .build();
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+            CommonResponse.builder()
+                .httpCode(HttpStatus.CREATED.value())
+                .data("댓글이 생성되었습니다")
+                .build());
+
     }
+
     @PatchMapping("/{commentId}")
-    public CommonResponse updateComment(@PathVariable Long commentId,@RequestBody CommentRequest request,
-        @AuthenticationPrincipal UserDetailsImpl userDetails){
-        commentService.updateComment(commentId,request,userDetails.getUser());
+    public ResponseEntity<CommonResponse> updateComment(@PathVariable Long commentId, @PathVariable Long boardId,
+        @RequestBody CommentRequest request,
+        @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        commentService.updateComment(commentId, boardId, request, userDetails.getUser());
 
-        return CommonResponse.builder()
-            .httpCode(204)
-            .build();
+        return ResponseEntity.status(HttpStatus.OK).body(
+            CommonResponse.builder()
+                .httpCode(HttpStatus.OK.value())
+                .data("댓글이 수정되었습니다")
+                .build());
     }
-    @DeleteMapping("/{commentId}")
-    public CommonResponse deleteComment(@PathVariable Long commentId,@AuthenticationPrincipal UserDetailsImpl userDetails){
-        commentService.deleteComment(commentId, userDetails.getUser());
 
-        return CommonResponse.builder()
-            .httpCode(204)
-            .build();
+    @DeleteMapping("/{commentId}")
+    public ResponseEntity<CommonResponse> deleteComment(@PathVariable Long commentId, @PathVariable Long boardId,
+        @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        commentService.deleteComment(commentId, boardId, userDetails.getUser());
+
+        return ResponseEntity.status(HttpStatus.OK).body(
+            CommonResponse.builder()
+                .httpCode(HttpStatus.OK.value())
+                .data("댓글이 삭제되었습니다")
+                .build());
     }
 
 
--- src/main/java/com/sparta/trello/domain/user/controller/UserController.java
+++ src/main/java/com/sparta/trello/domain/user/controller/UserController.java
@@ -9,10 +9,13 @@ import com.sparta.trello.global.security.UserDetailsImpl;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,30 +31,36 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    public CommonResponse signup(@Valid @RequestBody SignupRequest signupRequest) {
+    public ResponseEntity<CommonResponse> signup(@Valid @RequestBody SignupRequest signupRequest) {
         userService.signup(signupRequest);
 
-        return CommonResponse.builder()
-            .httpCode(201)
-            .build();
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+            CommonResponse.builder()
+                .httpCode(HttpStatus.CREATED.value())
+                .data(signupRequest.getUsername()+"의 회원가입이 완료되었습니다.")
+                .build());
     }
 
     @GetMapping()
-    public CommonResponse<Object> showProfile(@AuthenticationPrincipal UserDetailsImpl userDetails){
+    public ResponseEntity<CommonResponse<Object>> showProfile(
+        @AuthenticationPrincipal UserDetailsImpl userDetails){
         InfoResponse response = userService.showProfile(userDetails.getUser());
 
-        return CommonResponse.builder()
-            .httpCode(201)
-            .data(response)
-            .build();
+        return ResponseEntity.status(HttpStatus.OK).body(
+            CommonResponse.builder()
+                .httpCode(HttpStatus.OK.value())
+                .data(response)
+                .build());
     }
     @PatchMapping()
-    public CommonResponse<Object> updateProfile(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody @Valid InfoRequest profileRequest){
+    public ResponseEntity<CommonResponse> updateProfile(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody @Valid InfoRequest profileRequest){
         userService.updateProfile(userDetails.getUser(),profileRequest);
 
-        return CommonResponse.builder()
-            .httpCode(204)
-            .build();
+        return ResponseEntity.status(HttpStatus.OK).body(
+            CommonResponse.builder()
+                .httpCode(HttpStatus.OK.value())
+                .data("회원정보가 수정되었습니다.")
+                .build());
     }
 
 

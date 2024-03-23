cb18a9f1d6fd0c6168fd49ebade3729924ead198
DAchoMI <dachomi97@gmail.com>
2024-03-23 15:57:48 +0900

    :sparkles: ColumnController 에 Swagger 설명 추가

--- src/main/java/com/sparta/trello/domain/column/controller/ColumnController.java
+++ src/main/java/com/sparta/trello/domain/column/controller/ColumnController.java
@@ -6,6 +6,8 @@ import com.sparta.trello.domain.column.dto.ModifyColumnNameRequest;
 import com.sparta.trello.domain.column.dto.ModifyColumnSequenceRequest;
 import com.sparta.trello.domain.column.service.ColumnService;
 import com.sparta.trello.global.dto.CommonResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,6 +22,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Column API", description = "컬럼 API")
 @Slf4j
 @RestController
 @RequiredArgsConstructor
@@ -28,6 +31,7 @@ public class ColumnController {
 
     private final ColumnService columnService;
 
+    @Operation(summary = "컬럼 생성", description = "입력된 boardId에 컬럼을 생성합니다.")
     @PostMapping("/boards/{boardId}/columns")
     public ResponseEntity<CommonResponse<ColumnResponse>> createColumn(
         @PathVariable Long boardId,
@@ -40,6 +44,7 @@ public class ColumnController {
                 .httpCode(HttpStatus.CREATED.value()).data(response).build());
     }
 
+    @Operation(summary = "컬럼 이름 수정", description = "입력된 columnId의 이름을 수정합니다.")
     @PatchMapping("/boards/{boardId}/columns/{columnId}")
     public ResponseEntity<CommonResponse<ColumnResponse>> modifyColumnName(
         @PathVariable Long boardId,
@@ -53,6 +58,7 @@ public class ColumnController {
                 .httpCode(HttpStatus.NO_CONTENT.value()).data(response).build());
     }
 
+    @Operation(summary = "컬럼 삭제", description = "입력된 columnId를 삭제합니다.")
     @DeleteMapping("/boards/{boardId}/columns/{columnId}")
     public ResponseEntity<CommonResponse<Void>> deleteColumn(
         @PathVariable Long boardId,
@@ -65,6 +71,7 @@ public class ColumnController {
                 .httpCode(HttpStatus.NO_CONTENT.value()).build());
     }
 
+    @Operation(summary = "컬럼 순서 수정", description = "입력된 columnId의 순서를 수정합니다.")
     @PatchMapping("/boards/{boardId}/columns/{columnId}/sequence")
     public ResponseEntity<CommonResponse<ColumnResponse>> modifyColumnSequence(
         @PathVariable Long boardId,
@@ -78,6 +85,7 @@ public class ColumnController {
                 .httpCode(HttpStatus.NO_CONTENT.value()).data(response).build());
     }
 
+    @Operation(summary = "컬럼 조회", description = "컬럼을 순서 정렬해 조회합니다.")
     @GetMapping("/boards/{boardId}/columns")
     public ResponseEntity<CommonResponse<List<ColumnResponse>>> getColumnsOrderBySequence(
         @PathVariable Long boardId

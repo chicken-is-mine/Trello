6a71ac1384f037dc958b41d66ba26b80ddd774a4
jehyun <jehyun1590@naver.com>
2024-03-23 16:37:12 +0900

    :fire: :미사용 메서드 삭제

--- src/main/java/com/sparta/trello/domain/board/repository/BoardRepositoryImpl.java
+++ src/main/java/com/sparta/trello/domain/board/repository/BoardRepositoryImpl.java
@@ -3,17 +3,14 @@ package com.sparta.trello.domain.board.repository;
 
 import static com.sparta.trello.domain.board.entity.QBoard.board;
 import static com.sparta.trello.domain.board.entity.QBoardUser.boardUser;
+import static com.sparta.trello.domain.card.entity.QCard.card;
+import static com.sparta.trello.domain.column.entity.QColumns.columns;
 import static com.sparta.trello.domain.user.entity.QUser.user;
 
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sparta.trello.domain.board.dto.BoardInfo;
 import com.sparta.trello.domain.board.entity.Board;
-import com.sparta.trello.domain.board.entity.QBoard;
-import com.sparta.trello.domain.board.entity.QBoardUser;
-import com.sparta.trello.domain.card.entity.QCard;
-import com.sparta.trello.domain.column.entity.QColumns;
-import com.sparta.trello.domain.user.entity.QUser;
 import com.sparta.trello.domain.user.entity.User;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -29,10 +26,10 @@ public class BoardRepositoryImpl implements CustomBoardRepository {
     @Override
     public List<User> findExistingMemberByBoard(Board board) {
         return queryFactory
-            .select(QUser.user)
-            .from(QBoardUser.boardUser)
-            .join(QBoardUser.boardUser.user, QUser.user)
-            .where(QBoardUser.boardUser.board.eq(board))
+            .select(user)
+            .from(boardUser)
+            .join(boardUser.user,user)
+            .where(boardUser.board.eq(board))
             .fetch();
     }
 
@@ -59,16 +56,16 @@ public class BoardRepositoryImpl implements CustomBoardRepository {
                 .where(boardUser.board.boardId.eq(boardId))
                 .execute();
 
-        queryFactory.delete(QCard.card)
-                .where(QCard.card.column.board.boardId.eq(boardId))
+        queryFactory.delete(card)
+                .where(card.column.board.boardId.eq(boardId))
                 .execute();
 
-        queryFactory.delete(QColumns.columns)
-                .where(QColumns.columns.board.boardId.eq(boardId))
+        queryFactory.delete(columns)
+                .where(columns.board.boardId.eq(boardId))
                 .execute();
 
-        queryFactory.delete(QBoard.board)
-                .where(QBoard.board.boardId.eq(boardId))
+        queryFactory.delete(board)
+                .where(board.boardId.eq(boardId))
                 .execute();
 
     }
